2cdf1f795d9d3bb333b0fd81fa34f44b8c0f41af
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-09-11 18:21:28 +0800

    update

--- lib/pages/home/responsive/mobile/mobile_home.dart
+++ lib/pages/home/responsive/mobile/mobile_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MobileHome extends StatelessWidget {
   const MobileHome({super.key});
@@ -8,6 +9,14 @@ class MobileHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Mobile Layout Size: ${MediaQuery.of(context).size.width}'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.searchengin), label: 'Pencarian'),
+        ],
       ),
     );
   }
