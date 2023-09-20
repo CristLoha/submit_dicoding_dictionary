7983a999956b39b3d9da27c13bdd12c672dd7a68
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-09-20 08:54:16 +0800

    update

--- lib/pages/detail_hewan/responsive/mobile_detail.dart
+++ lib/pages/detail_hewan/responsive/mobile_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../shared/theme.dart';
 
 class MobileDetail extends StatelessWidget {
   const MobileDetail({super.key});
@@ -6,11 +7,45 @@ class MobileDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: lightBackgroundColor,
       appBar: AppBar(
-        title: Text('Detail'),
+        elevation: 0,
+        iconTheme: IconThemeData(color: whiteColor),
+        title: const Text('Hewan'),
+        titleTextStyle: whiteTextStyle.copyWith(
+          fontWeight: semiBold,
+          fontSize: 20,
+        ),
       ),
-      body: ListView(
-        children: [],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+              color: shamrockGreen,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 30,
+                  horizontal: 24,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Banyo',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 20,
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [],
+            ),
+          ),
+        ],
       ),
     );
   }
