1ec3aa96cae24f845f160200412a876da2a57bd7
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-10-02 17:00:31 +0800

    update

--- lib/widgets/text_underline.dart
+++ lib/widgets/text_underline.dart
@@ -11,7 +11,8 @@ class UnderlineText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      /// Hanya menggunakan font default, karena font poppins tidak mendukung fitur ini
+      /// Hanya menggunakan font default, karena font poppins tidak bisa diberi underline
+      /// Tujuan fitur ini dibuat, agar bahasa daerah yang memiliki karakter yang unik, maka diberi underline atau garis bawah, khususnya bahasa Sahu.
       text.replaceAllMapped(
         RegExp(r'_\w'),
         (m) => '${m.group(0)![1]}̲',
